2a260885b8a9aec696b1e29ee5396375fbaa54b7
inthumaktics <inthumakticss@gmail.com>
2025-12-24 01:06:59 +0700

    fix github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DED-kost_Project_TEKWEB/',
   plugins: [react()],
-  base: '/DED-kost_Project_TEKWEB',
 })
