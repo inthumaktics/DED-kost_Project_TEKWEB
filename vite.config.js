3e94373c866c27343bf11bca67c7c2955f9ab7dc
inthumaktics <inthumakticss@gmail.com>
2026-01-14 07:57:23 +0700

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/DED-kost_Project_TEKWEB/',
+  base : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
